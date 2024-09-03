acea588b11f220965160fef9e27c6ea1281c77bb
JHKINMS <wjdgns5131@gmail.com>
2024-09-03 20:47:56 +0900

    toy: 간단한 이벤트 처리

--- front/App.tsx
+++ front/App.tsx
@@ -5,21 +5,23 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, SafeAreaView, StyleSheet,Text,TextInput,View } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [name, setName] = useState('');
+
+  const handleChangeInput = (text: string) => {
+    setName(text);
+  }
   return (
     // <SafeAreaView></SafeAreaView>는 노치 영역을 침범하지 않는 스타일을 위한 것임.
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input}/>
-        <Text>Test</Text>
+        <TextInput style={styles.input} onChangeText={handleChangeInput} value={name}/>
+        <Text>이름</Text>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input}/>
-        <Text>Test</Text>
-      </View>
+      
     </SafeAreaView>
   )
 }
@@ -34,7 +36,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'black',
     width: 100,
-    height: 100,
+    height: 50,
   },
   inputContainer:{
     flex: 1,
